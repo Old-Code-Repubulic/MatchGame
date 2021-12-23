9a984ef6df92e9ad5069d0a03957e6a7151bfe04
joseph tidwell <joseph.tidwell09@gmail.com>
2021-12-22 20:10:02 -0600

    named grid in xaml to be referenced in code behind

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<String> animalEmoji = new List<string>()
+            {
+            "🤡","🤡",
+            "👺","👺",
+            "👽","👽",
+            "🙀","🙀",
+            "🙉","🙉",
+            "🐺","🐺",
+            "🐷","🐷",
+            "🐲","🐲"
+            };
+
+            Random random = new Random();
+
+            foreach(TextBlock textblock in mainGrid.Children.OfType<TextBlock>());
+        }
+    }
+}
